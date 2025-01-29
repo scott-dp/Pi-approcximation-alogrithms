e8dc69a8e60eb6d4cf40d2aee7d4e5328eceaac4
Scott <scottld@stud.ntnu.no>
2025-01-29 15:08:55 +0100

    Made constructor for simulation base class and added the first simulation class

---
+++ src/MonteCarloApproximation.cpp
@@ -0,0 +1,10 @@
+//
+// Created by scott on 29.01.2025.
+//
+
+#include "../include/MonteCarloApproximation.h"
+
+MonteCarloApproximation::MonteCarloApproximation(int amountOfBitsPrecision) : PiApproximationAlgorithm(
+        amountOfBitsPrecision) {
+
+}
--- src/PiApproximationAlgorithm.cpp
+++ src/PiApproximationAlgorithm.cpp
@@ -1,5 +1,17 @@
 //
 // Created by scott on 29.01.2025.
 //
-
+#include <gmp.h>
 #include "../include/PiApproximationAlgorithm.h"
+
+PiApproximationAlgorithm::PiApproximationAlgorithm(int amountOfBitsPrecision) {
+    mpf_set_default_prec(amountOfBitsPrecision); //Set precision to given amount of bits
+    mpf_init(piApproximated);
+    mpf_init(piCorrect);
+    mpf_set_str(piCorrect, "3.141592653589793238462643383279502884197169399375105820974944", 10); // Base 10 string representation
+}
+
+PiApproximationAlgorithm::~PiApproximationAlgorithm() {
+    mpf_clear(piApproximated);
+    mpf_clear(piCorrect);
+}
