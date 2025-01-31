cfa3ba0608d7ce43d831d674039a6eff3c0b8de9
Scott <scottld@stud.ntnu.no>
2025-01-31 15:33:43 +0100

    Refactoring main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@
 #include <iostream>
 
 using namespace std;
+void testMonteCarloApproximation();
+void testMachinsFormulaApproximation();
 
 int main() {
-    /**
+    testMonteCarloApproximation();
+    testMachinsFormulaApproximation();
+    return 0;
+}
+
+void testMonteCarloApproximation() {
     //Run monte carlo with 10 million iterations
     //2000 bits of precision would be about 600 decimals in base10
     MonteCarloApproximation monteCarloPiApproximator(2000);
@@ -17,13 +24,17 @@ int main() {
 
     // Calculate the duration
     chrono::duration<double> duration = end - start;
-
     // Output the time in seconds
-    cout << "Execution time: " << duration.count() << " seconds.\n";
-     */
+    cout << "Execution time monte carlo: " << duration.count() << " seconds.\n";
+    cout << "Monte carlo amount of correct digits: " << monteCarloPiApproximator.findAmountCorrectDigitsOfPi() <<endl;
+}
+
+void testMachinsFormulaApproximation() {
     MachinsFormulaPiApproximation machinsFormulaPiApproximator(2000);
+    auto start = chrono::high_resolution_clock::now();
     machinsFormulaPiApproximator.calculate(10);
-    int digitsSame = machinsFormulaPiApproximator.findAmountCorrectDigitsOfPi();
-    cout << digitsSame << endl;
-    return 0;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "Execution time machins formula: " << duration.count() << " seconds.\n";
+    cout << "Machins formula amount of correct digits: " << machinsFormulaPiApproximator.findAmountCorrectDigitsOfPi() << endl;
 }
